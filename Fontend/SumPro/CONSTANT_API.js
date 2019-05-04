8f41704ce8dd2369109949329914426e0126c4f7
Saleban Olow <olow304@gmail.com>
2019-05-04 15:36:52 -0400

    defined const vars outside the main file

---
+++ Fontend/SumPro/CONSTANT_API.js
@@ -0,0 +1,4 @@
+const EXTRACTIVE_API = "http://127.0.0.1:5000/extractive"
+const HELPER_API = "https://api.aylien.com/api/v1/extract"
+const SAVE_SUMMARY_API = "http://127.0.0.1:8086/get-summary"
+const RIGHT_CLICK_API = "http://localhost:8087/summary?selected="
